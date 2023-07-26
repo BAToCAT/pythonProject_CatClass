cb794b9991f061337bbc3c06df80e04953d97d32
BAToCAT <prosto2x2@gmail.com>
2023-07-26 20:09:57 +0300

    Initial commit

---
+++ Cat.py
@@ -0,0 +1,15 @@
+class Cats:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def Cat_name(self):
+        return self.name
+
+    def Cat_gender(self):
+        return self.gender
+
+    def Cat_age(self):
+        return self.age
+
---
+++ CatObj.py
@@ -0,0 +1,10 @@
+from Cat import Cats
+
+Cat_1 = Cats('Барон', 'мальчик', '2')
+Cat_2 = Cats('Сэм', 'мальчик', '2')
+
+
+print('Имя    / пол     / возраст')
+print(Cat_1.Cat_name(),'/', Cat_1.Cat_gender(),'/', Cat_1.Cat_age())
+print("__ __ __")
+print(Cat_2.Cat_name(),'/', Cat_2.Cat_gender(),'/', Cat_2.Cat_age())
